8d389134b8d6d305a3e1ceedefb389544e1a104a
Ksr <ksl1580@naver.com>
2022-10-21 16:01:21 +0900

    ver1.2

---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <div>
+            <h1>여기가 홈 입니다.</h1>
+            <p>가장 먼저 보여지는 페이지 입니다.</p>
+        </div>
+    )
+}
+export default Login;
--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 
 const Container = styled.div`
-    
+    width:1920px ;
     background-color: black;
     color: white;
     
