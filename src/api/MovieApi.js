ba016a22e9d0607e8fe291ba2c48579bc23ad014
jimin0601 <109451342+jimin0601@users.noreply.github.com>
2022-10-28 16:57:14 +0900

    회원가입 api 테스트 끝

--- src/api/MovieApi.js
+++ src/api/MovieApi.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 const HEADER = 'application/json';
-const MOVIE_DOMAIN="http://localhost:8090/Movie_login/";
+const MOVIE_DOMAIN="http://cokebear756.synology.me:62322/api/client/";
 
 const MovieApi={
+
     // 로그인 api
     userLogin : async function(inputId, inputPwd){
         const loginObj = {
@@ -12,14 +13,14 @@ const MovieApi={
         return await axios.post(MOVIE_DOMAIN + "LoginServlet", loginObj, HEADER)
     },
     // 회원가입 api
-    memberReg : async function(inputId, inputPwd, inputName,inputEmail){
+    memberReg : async function(inputId, inputPwd, inputName ,inputEmail){
         const memberObj = {
             id : inputId,
-            pwd : inputPwd,
+            password : inputPwd,
             name : inputName,
             email : inputEmail
         }
-        return await axios.post(MOVIE_DOMAIN + "MemberRegServlet", memberObj, HEADER)
+        return await axios.post(MOVIE_DOMAIN + "member/signup", memberObj, HEADER)
     },
     // 회원가입 여부 api
     memberRegCheck : async function(inputId){
@@ -27,14 +28,6 @@ const MovieApi={
             id : inputId,
         }
         return await axios.post(MOVIE_DOMAIN + "IdRegCheck", regCheck, HEADER)
-    },
-    // 비밀번호 찾기 api
-    findPassword : async function(inputId, inputEmail){
-        const finding ={
-            id : inputId,
-            email : inputEmail
-        }
-        return await axios.post(MOVIE_DOMAIN + "Find", finding, HEADER)
     },
     // 현재 비밀번호가 존재하는지 확인하기 위한 API
     nowPwdCheck : async function(regPwd) {
@@ -49,6 +42,13 @@ const MovieApi={
             newPwd : newPwd
         }
         return await axios.post(MOVIE_DOMAIN + "SetPwdServlet", setPwd, HEADER);
+    },
+    findPwd : async function(id, email) {
+        const findPwd = {
+            id : id,
+            email : email
+        }
+        return await axios.post(MOVIE_DOMAIN + "PwdFineServlet", findPwd, HEADER);
     }
 }
 export default MovieApi;
