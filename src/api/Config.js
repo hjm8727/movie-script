bd4c0f30bb279769a6259fe00647acaf6f9fef91
hjm8727 <hjm8727@gmail.com>
2022-10-31 16:45:04 +0900

    main

--- src/api/Config.js
+++ src/api/Config.js
@@ -1,5 +1,5 @@
 
-export const API_URL = 'https://api.themoviedb.org/3/'
+export const API_URL = 'http://cokebear756.synology.me:62322/api'
 
 export const API_KEY = '861d4deb025a7b74ed50b08231be3593'
 
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {API_URL, API_KEY, IMAGE_BASE_URL} from '../../api/Config';
+import {API_URL, IMAGE_BASE_URL} from '../../api/Config';
 import MainImage from './section/MainImage';
 import GridCards from '../Cards/GridCards';
 import {Row} from 'antd';
@@ -7,17 +7,26 @@ import {Row} from 'antd';
 function MainPage() {
 
     const [Movies, setMovies] = useState([])
+    const [Movies2, setMovies2] = useState([])
+    const [Movies3, setMovies3] = useState([])
     const [MainMovieImage, setMainMovieImage] = useState(null)
     const [CurrentPage, setCurrentPage] = useState(0)
 
     // TMDB API 사용중 추후 DB API로 수정 예정
     useEffect(() => {
-        const content = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko-KR&page=1&size=10`;
-        FetchMovies(content)        
+        const nowPlaying = `${API_URL}/movie/nowPlaying?page=0&size=10`;
+        FetchMovies(nowPlaying)
+        const topRated = `${API_URL}/movie/topRated?page=0&size=10`;
+        FetchMovies2(topRated)
+        const upcoming = `${API_URL}/movie/upcoming?page=0&size=10`;
+        FetchMovies3(upcoming)
     }, [])
 
-    const FetchMovies = (content) => {
-        fetch(content)
+    const FetchMovies = (nowPlaying) => {
+        fetch(nowPlaying, {
+            method : "POST",
+            body: JSON.stringify(Movies)
+        })
         .then(response => response.json())
         .then(response => {
         console.log(response)
@@ -26,10 +35,45 @@ function MainPage() {
         setCurrentPage(response.page)
         })
     }
+    const FetchMovies2 = (topRated) => {
+        fetch(topRated, {
+            method : "POST",
+            body: JSON.stringify(Movies2)
+        })
+        .then(response => response.json())
+        .then(response => {
+        console.log(response)
+        setMovies2([...Movies2,...response.results])
+        setMainMovieImage(response.results[0])
+        setCurrentPage(response.page)
+        })
+    }
+    const FetchMovies3 = (upcoming) => {
+        fetch(upcoming, {
+            method : "POST",
+            body: JSON.stringify(Movies3)
+        })
+        .then(response => response.json())
+        .then(response => {
+        console.log(response)
+        setMovies3([...Movies3,...response.results])
+        setMainMovieImage(response.results[0])
+        setCurrentPage(response.page)
+        })
+    }
+
 
     const loadMore = () => {
-        const content = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko-KR&page=${CurrentPage +1}`;
-        FetchMovies(content)
+        const nowPlaying = `${API_URL}/movie/nowPlaying?page=${CurrentPage +1}&size=10`;
+        FetchMovies(nowPlaying)
+    }
+    const loadMore2 = () => {
+        const topRated = `${API_URL}/movie/topRated?page=${CurrentPage +1}&size=10`;
+        FetchMovies2(topRated)
+    }
+    const loadMore3 = () => {
+        const upcoming = `${API_URL}/movie/upcoming?page=${CurrentPage +1}&size=10`;
+        FetchMovies3(upcoming)
     }
 
     return (
@@ -48,16 +92,49 @@ function MainPage() {
             <Row gutter={[16, 16]}>
             {Movies && Movies.map((movie, index) => (
                 <React.Fragment key={index}>
-                <GridCards MainPage image={movie.poster_path ? `${IMAGE_BASE_URL}w500${movie.poster_path}` : null} movieId={movie.id} movieName={movie.title}/>
+                <GridCards MainPage image={movie.file_path ? `${IMAGE_BASE_URL}w500${movie.file_path}` : null} movieId={movie.id} movieName={movie.title}/>
                 </React.Fragment>
             ))}
             </Row>
         </div>
-
         {/* 더보기 */}
         <div style={{display: 'flex', justifyContent: 'center' }}>
             <button style={{backgroundColor: '#FFD369', color: 'black'}} onClick={loadMore}>더 보기</button>
         </div>
+
+        {/* 카테고리 부분 */}
+        <div style={{width: '85%', margin: '1rem auto'}}>
+            <h2 style={{color: '#FFD369'}}>최고 평점 영화</h2>
+            <hr/>
+            <Row gutter={[16, 16]}>
+            {Movies2 && Movies2.map((movie, index) => (
+                <React.Fragment key={index}>
+                <GridCards MainPage image={movie.file_path ? `${IMAGE_BASE_URL}w500${movie.file_path}` : null} movieId={movie.id} movieName={movie.title}/>
+                </React.Fragment>
+            ))}
+            </Row>
+        </div>
+        {/* 더보기 */}
+        <div style={{display: 'flex', justifyContent: 'center' }}>
+            <button style={{backgroundColor: '#FFD369', color: 'black'}} onClick={loadMore2}>더 보기</button>
+        </div>
+
+        {/* 카테고리 부분 */}
+        <div style={{width: '85%', margin: '1rem auto'}}>
+            <h2 style={{color: '#FFD369'}}>개봉 예정작</h2>
+            <hr/>
+            <Row gutter={[16, 16]}>
+            {Movies3 && Movies3.map((movie, index) => (
+                <React.Fragment key={index}>
+                <GridCards MainPage image={movie.file_path ? `${IMAGE_BASE_URL}w500${movie.file_path}` : null} movieId={movie.id} movieName={movie.title}/>
+                </React.Fragment>
+            ))}
+            </Row>
+        </div>
+        {/* 더보기 */}
+        <div style={{display: 'flex', justifyContent: 'center' }}>
+            <button style={{backgroundColor: '#FFD369', color: 'black'}} onClick={loadMore3}>더 보기</button>
+        </div>                
         </div>
     );
 }
