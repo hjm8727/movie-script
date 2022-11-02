a05fc61348101ef74971de933c5836d43a94da1f
hjm8727 <hjm8727@gmail.com>
2022-11-02 15:52:38 +0900

    fix 70%

--- src/api/Config.js
+++
@@ -1,7 +0,0 @@
-
-export const API_URL = 'http://cokebear756.synology.me:62322/api'
-
-export const API_KEY = '861d4deb025a7b74ed50b08231be3593'
-
-export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
-
