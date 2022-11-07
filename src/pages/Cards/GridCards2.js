f1fa80afe623e85d8517fd6ca756c15cbe573a6f
hjm8727 <hjm8727@gmail.com>
2022-11-07 20:04:59 +0900

    hjm

--- src/pages/Cards/GridCards2.js
+++ src/pages/Cards/GridCards2.js
@@ -8,14 +8,15 @@ function GridCards2(props) {
     useEffect(() => { setLoading(false) }, [])
 
     return (
-        <div style={{display: 'inline-flex' , width: '250px'}}>
+        <div style={{display: 'inline-flex'}}>
         <Col lg={6} md={8} xs={24}  style={{paddingBottom: '10px'}}>
             <div style={{ position: 'relative' }}>
             {Loading && <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}><NowLoading/></div>}
-            <img style={{ width: '245px', height: '320px', border: '2px solid #FFD369'}} src={props.image} alt={NoImage} />
+            <img style={{ width: '245px', height: '320px', border: '2px solid #FFD369'}} src={props.image} alt={NoImage}/>
             </div>
+            <div style={{width: '240px'}} >
             <b style={{color: '#FFD369'}}>{props.characterName} </b>
-            <br/><b style={{color: 'white'}} >as </b><b style={{color: '#FFD369'}}>{props.character}</b>
+            <br/><b style={{color: 'white'}} >as </b><b style={{color: '#FFD369'}}>{props.character}</b></div>
         </Col>
         </div>
     )
--- src/util/NoImage.js
+++ src/util/NoImage.js
@@ -1,14 +1,11 @@
-import Card from 'react-bootstrap/Card';
+
 
 // DB에 이미지가 없을시 보여주기 위한 박스
 function NoImage() {
   return (
-    <Card style={{ width: '245px', height: '320px', backgroundColor: 'FFD369'}}>
-      <Card.Body>
+    <div style={{ width: '245px', height: '320px', backgroundColor: 'FFD369'}}>
       <img src="/images/Logo.png" alt="Logo"/>
-        <Card.Title>이미지를 찾을수 없습니다.</Card.Title>
-      </Card.Body>
-    </Card>
+    </div>
   );
 }
 
